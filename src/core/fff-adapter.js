4c520afc2452cf1a5ba454facd972de00f548be2
havingautism <havingautism@outlook.com>
2026-05-11 01:31:42 +0800

    chore: update version to 0.5.3 in deployment guide, package.json, package-lock.json, and fff-adapter.js

--- src/core/fff-adapter.js
+++ src/core/fff-adapter.js
@@ -111,7 +111,7 @@ class FffMcpClient {
       capabilities: {},
       clientInfo: {
         name: 'codemini-cli',
-        version: '0.5.2'
+        version: '0.5.3'
       }
     });
     this.sendNotification('notifications/initialized', {});
